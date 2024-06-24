93b8e8dd92b2bd5c04ea63e3e3ade7c1ff655874
KatelineLeBourdonnec <kateline.le-bourdonnec@u-bordeaux.fr>
2024-06-24 15:22:25 +0200

    add

--- R/test.R
+++ R/test.R
@@ -3,24 +3,14 @@ getwd()
 Paq1000 <- read.csv("data/Paq1000.txt", sep="")
 
 head(Paq1000)
-source("R/idm.R")
 library(survival)
-
 library(marqLevAlg)
 library(splines2)
-#library(SmoothHazardoptim9) #package Ariane
 library(prodlim)
 library(dplyr)
 library(survival)
-#library(SmoothHazard) #package Pierre
 library(ggplot2)
 library(doBy) 
-#pour enregistrer les objets
-#load("fitspline.Rdata")
-#save(fitsplinecens,file="fitsplinecens.Rdata")
-#save(fitspline,file="fitspline.Rdata")
-source("R/idm.weib.R")
-dyn.load("src/idmlikelihood.f90")
 
 
 
@@ -29,6 +19,8 @@ fit <- idm(
   
   formula01 = Hist(time = r, event = dementia) ~ certif, 
   
-  formula12 = ~ certif, data = Paq1000, nproc=3,clustertype = "PSOCK")
+  formula12 = ~ certif, data = Paq1000, nproc=3,clustertype = "PSOCK", timedep12=F)
 
 summary(fit)
+
+idm()
