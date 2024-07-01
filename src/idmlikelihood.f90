fedef5a6bbc16a4d0cc88c54cedc82a6a0dc2a2d
KatelineLeBourdonnec <kateline.le-bourdonnec@u-bordeaux.fr>
2024-07-01 21:52:38 +0200

    add

--- src/idmlikelihood.f90
+++ src/idmlikelihood.f90
@@ -456,10 +456,10 @@ end if
                     else   
                        if(c(i).eq.4)then ! cpi 0-->1 et obs 1-->2
 			print *, 'Cas 4'
-			print *, 'donnees = ', c(i)
-			print *, 'T1 = ', t1(i)
-			print *, 'T2 = ', t2(i)
-			print *, 'T3 = ', t3(i)
+			print *, 'donnees = ', c
+			print *, 'The01 = ', the01
+	!		print *, 'T2 = ', t2(i)
+	!		print *, 'T3 = ', t3(i)
                         
                         call  qgauss1(c(i),t1(i),t2(i),t3(i),the01,the02,the12,&
                         res2,vet01,vet02,vet12, gamma)
