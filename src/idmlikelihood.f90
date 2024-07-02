3297583b9922a3a33482eacc88e9480695c56052
KatelineLeBourdonnec <kateline.le-bourdonnec@u-bordeaux.fr>
2024-07-02 15:03:34 +0200

    add

--- src/idmlikelihood.f90
+++ src/idmlikelihood.f90
@@ -699,7 +699,7 @@ else
                 f2 = (su01**v01) * (su02**v02) * (su12_T**v12) * ri01 * v01 * ri12_T * v12 / (su12**v12)
                
                
-               print *, 'Test cas 4 Qgauss', cas, xx, c
+               print *, 'Test cas 4 Qgauss', cas, xx, c, v12, su12_T, su12
                
                 res = res + w(j) * (f1 + f2)
             end do
